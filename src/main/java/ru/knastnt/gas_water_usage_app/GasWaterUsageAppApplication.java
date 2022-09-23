508faa7331fad2b3fee023667aa44530bad3f57f
knastnt <456kot@mail.ru>
2022-09-24 09:29:30 +1000

    init

---
+++ src/main/java/ru/knastnt/gas_water_usage_app/GasWaterUsageAppApplication.java
@@ -0,0 +1,13 @@
+package ru.knastnt.gas_water_usage_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasWaterUsageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasWaterUsageAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/knastnt/gas_water_usage_app/GasWaterUsageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.knastnt.gas_water_usage_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasWaterUsageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
